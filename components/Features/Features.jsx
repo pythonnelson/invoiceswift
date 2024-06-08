3940f3efa599a6552bbc19f1358c3d699a4beca7
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2024-06-08 14:56:14 +0100

    Fix - Hover Background color in Features Component

--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -18,13 +18,13 @@ const Features = () => {
           return (
             <div
               key={index}
-              className="py-3 shadow mb-6 px-3 hover:bg-fuchsia-500 hover:rounded-md duration-300 transition-all"
+              className="py-3 shadow mb-6 px-3 hover:bg-white hover:rounded-md duration-300 transition-all"
             >
               <div className="text-[#1b6a88] rounded-md py-3 px-4">
                 <Icon className="w-8 h-8" />
               </div>
               <h2 className="mb-3 font-bold">{feature.title}</h2>
-              <p className="text-slate-500 hover:text-[#f1f1f1] duration-300 transition-all">
+              <p className="text-slate-500 duration-300 transition-all">
                 {feature.description}
               </p>
             </div>
