0cdcafff0de0069011031355dade9d84c851e642
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2024-06-08 12:36:11 +0100

    Implement shadow in features

--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -5,7 +5,7 @@ const Features = () => {
   return (
     <div className="py-8 md:py-16 px-4 md:px-16 bg-[#f1f1f1]">
       <h1 className="text-center text-2xl md:text-4xl font-semibold mb-6">
-        Features of InvoiceSwfit
+        Features of InvoiceSwift
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-4">
         {IFeatures.map((feature, index) => {
@@ -13,7 +13,7 @@ const Features = () => {
           return (
             <div
               key={index}
-              className="py-3 mr-4 hover:bg-fuchsia-500 hover:px-2 hover:rounded-md duration-300 transition-all"
+              className="py-3 mr-4 shadow mb-6 px-2 hover:bg-fuchsia-500 hover:px-2 hover:rounded-md duration-300 transition-all"
             >
               <div className="text-[#1b6a88] rounded-md py-3 px-4">
                 <Icon className="w-8 h-8" />
